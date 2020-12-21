7045bf95cfa7b821f7d6395c46a9c5e090a90a29
hey-irh <isabelholland90@gmail.com>
2020-12-21 18:26:25 +0000

    progress bar static

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch,Route,Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
-import DeleteButton from "./Components/Buttons/DeleteButton";
-import FavoriteButton from "./Components/Buttons/FavouriteButton";
-import MaterialUIPickers from "./Components/Input/DateInput";
-import SimpleBottomNavigation from "./Components/NavBar"
-import SubmitButton from "./Components/Buttons/SubmitButton"
+import DeleteButton from './Components/Buttons/DeleteButton';
+import FavoriteButton from './Components/Buttons/FavouriteButton';
+import MaterialUIPickers from './Components/Input/DateInput';
+import SimpleBottomNavigation from './Components/NavBar';
+import SubmitButton from './Components/Buttons/SubmitButton';
+import ProgressBar from './Components/ProgressBar';
 
 function App() {
   return (
-    <div className="App">
-
-
-    <p>Hello World</p> 
-    <DeleteButton deleteColor="#f39a9d"/>
-    <DeleteButton deleteColor="#3f6c51"/>
-    <FavoriteButton favoriteColor="#301a4b"/>
-    <FavoriteButton favoriteColor="red"/>
-    <SubmitButton submitColor="#f39a9d"/>
-    <MaterialUIPickers />
-    <SimpleBottomNavigation />
+    <div className='App'>
+      <p>Hello World</p>
+      <DeleteButton deleteColor='#f39a9d' />
+      <DeleteButton deleteColor='#3f6c51' />
+      <FavoriteButton favoriteColor='#301a4b' />
+      <FavoriteButton favoriteColor='red' />
+      <SubmitButton submitColor='#f39a9d' />
+      <MaterialUIPickers />
+      <SimpleBottomNavigation />
     </div>
   );
 }
