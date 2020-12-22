5ae4ba95f2f29d2a0625d2dc603929418f571d15
= <=>
2020-12-22 15:06:52 +0000

    push mood attached to backend

--- src/Components/3.Emotions_Page/index.js
+++ src/Components/3.Emotions_Page/index.js
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from 'react';
 import EmotionsButton from "../Buttons/EmotionsButtons";
 import quoteData from "./quotesData.js";
 
+//Temp userId
+const userId = 1;
+
 function Emotions() {
   // need user_id from ContextProvider
 
   //need to figure out how to close the ability to click for the day/only enable one click per day
   const emotionsArray = [{emotion: "😢", number: 1},{emotion: "😒", number: 2},{emotion: "😬", number: 3},{emotion: "😀", number: 4},{emotion: "😍", number: 5} ]
-  const [chosenEmotion, setChosenEmotion] = useState(0);
+  const [chosenEmotion, setChosenEmotion] = useState(null);
   
   function handleEmotion(emotionNum){
     console.log("running");
@@ -17,28 +20,29 @@ function Emotions() {
 
   // We need to connect to the Database via a valid URL & Need to get the userID & name from the context provider
 
-//   useEffect(() => {
-//     if (!chosenEmotion) {
-//       return;
-//     }
-//   async function postEmotion() {
-//     const res = await fetch(
-//       // neeed to actual API address
-//       `https://localhost:3000/moods`,
-//       {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//           user_id: userId, //context provider object.user_id
-//           mood: chosenEmotion,
-//         })
-//       })
-//     const data = await res.json();
-//     console.log(data);
-//     //hopefully returned a unique post numb
-//   }
-//   postEmotion();
-// }, [chosenEmotion]);
+  useEffect(() => {
+   if (chosenEmotion){
+
+  async function postEmotion() {
+    const res = await fetch(
+  
+      // neeed to actual API address
+      `http://localhost:3000/moods`,
+      {
+        method: "POST",
+        headers: { "content-type": "application/JSON"},
+        body: JSON.stringify({
+          user_id: userId, 
+          mood: chosenEmotion
+        })
+      })
+    const data = await res.json();
+    console.log(data);
+    //hopefully returned a unique post numb
+  }
+  postEmotion();
+}}, [chosenEmotion]);
+  
 
   return (
     <div>
