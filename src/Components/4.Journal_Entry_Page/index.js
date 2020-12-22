0c450d6540d55d4171e235f8fb0065e9731ad97c
Anna-Marie Moss <ambrown1976@googlemail.com>
2020-12-22 11:54:16 +0000

    journal entry page text area and file upload

--- src/Components/4.Journal_Entry_Page/index.js
+++ src/Components/4.Journal_Entry_Page/index.js
@@ -5,6 +5,22 @@ function JournalEntry() {
     <div>
       <h1>How was your day today?</h1>
       <h1>What did you learn?</h1>
+      <div className='container'>
+        <form>
+          <textarea
+            className='journalNote'
+            rows='4'
+            cols='60'
+            //add onchange event here
+            placeholder='Add your journal note here...'
+            type='text'
+          ></textarea>
+          <br></br>
+          <button type='submit'>Submit</button>
+          <input type='file' accept='image/x-png,image/jpeg,image/gif' />
+        </form>
+      </div>
+
       <p> (Add journal component card here)</p>
     </div>
   );
