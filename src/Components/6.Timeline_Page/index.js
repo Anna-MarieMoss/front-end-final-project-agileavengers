c013ec2ef81d67c13e19cae19c80c659f1fd2e02
hey-irh <isabelholland90@gmail.com>
2020-12-22 10:22:26 +0000

    framework done

---
+++ src/Components/6.Timeline_Page/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProgressBar from '../ProgressBar/index';
+
+function Timeline() {
+  return (
+    <div>
+      <h1>Your Timeline</h1>
+      <p> (Add in progress bar here)</p>
+      <p> (Add in progress congratulations message here)</p>
+      <ProgressBar />
+    </div>
+  );
+}
+
+export default Timeline;
