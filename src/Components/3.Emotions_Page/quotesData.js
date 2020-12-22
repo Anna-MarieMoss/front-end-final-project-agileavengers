f0a005df5df3ff09ecd913be32981aff164c3022
Anna-Marie Moss <ambrown1976@googlemail.com>
2020-12-22 10:34:57 +0000

    adding quotes sample data

---
+++ src/Components/3.Emotions_Page/quotesData.js
@@ -0,0 +1,34 @@
+const quotesData = [
+  {
+    quote:
+      'Every day may not be good but there is something good in every day!',
+  },
+  {
+    quote:
+      'When things get tough remember: better days are coming. They are called Saturday and Sunday.',
+  },
+  { quote: 'Choose to be optimistic. It feels better.' },
+  {
+    quote:
+      'Just in case no one told you today: Good morning! You are doing great. I believe in you. Nice butt.',
+  },
+  {
+    quote:
+      "Breathe. Don't let a bad day make you feel like you have a bad life.",
+  },
+  {
+    quote: 'A good laugh and a long sleep are the two best cures for anything.',
+  },
+  { quote: 'Do something today that you future self will thank you for.' },
+  { quote: 'Start where you are. Use what you have. Do what you can.' },
+  {
+    quote:
+      'Don’t wait for your feelings to change to take the action. Take the action and your feelings will change.',
+  },
+  {
+    quote:
+      'You measure the size of the accomplishment by the obstacles you had to overcome to reach your goals.',
+  },
+];
+
+export default quotesData;
