ddf2c66c51f8a115a279b34b5c1115f93e39f6c2
Anna-Marie Moss <ambrown1976@googlemail.com>
2020-12-22 10:05:53 +0000

    missing journalview page

---
+++ src/Components/5.Journal_View_Page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function JournalView() {
+  return (
+    <div>
+      <h1>Your story so far...</h1>
+      <p> (Display all journal component cards here)</p>
+      <p>(Add in filters for date range, emotions etc)</p>
+    </div>
+  );
+}
+
+export default JournalView;
