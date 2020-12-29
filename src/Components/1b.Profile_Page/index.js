cd9c26d62b483a6853ba3c3692fa116c8422fc6a
= <=>
2020-12-29 15:42:30 +0000

    Profile set up - next attach db

--- src/Components/1b.Profile_Page/index.js
+++ src/Components/1b.Profile_Page/index.js
@@ -1,22 +1,86 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import DatePicker from '../Input/DateInput/index.js';
+import H1 from '../DisplayText/H1Text';
+import SubmitButton from "../Buttons/SubmitButton/index";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function Profile() {
+  const classes = useStyles();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [myersBriggs, setMyersBriggs] = useState("")
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [submit, setSubmit] = useState(false)
+  console.log(name);
+  console.log(selectedDate)
+
+  function handleSubmit(){
+    setSubmit(true);
+  }
+  useEffect(() => {
+   if (submit === true){
+
+    async function postprofile() {
+      const res = await fetch(
+    
+        // neeed to actual API address
+        `http://localhost:3000/users`,
+        {
+          method: "POST",
+          headers: { "content-type": "application/JSON"},
+          body: JSON.stringify({
+            name: name, 
+            email: email,
+            password: "password",
+            personality: myersBriggs,
+            start_date: selectedDate,
+            points: 0
+          })
+        })
+      const data = await res.json();
+      console.log(data);
+      //hopefully returned a unique post numb
+    }
+    postprofile();
+    setSubmit(false);
+  }}, [submit]);
+
+
+ 
+
   return (
     <div>
-      <h1>Profile</h1>
+      <H1 text={"Profile"}></H1>
       <p>(Add profile img from Auth0 here)</p>
       <p>(Upload photo link here)</p>
-      <form>
-        <input className='userName' placeholder='Name' />
-        <br></br>
-        <input className='Email' placeholder='Email' />
-        <br></br>
-        <input className='MyersBriggs' placeholder='Myers Briggs' />
-        <br></br>
-        <input className='startDate' type='date' />
-        <br></br>
-        <button>Submit</button>
-      </form>
+      <form className={classes.root} noValidate autoComplete="off">
+    <div>
+    <TextField id="outlined-search" label="Name" type="text" variant="outlined" onChange={event => {
+          const { value } = event.target;
+          setName(value);
+        }}/>
+    <TextField id="outlined-search" label="Email" type="email" variant="outlined" onChange={event => {
+          const { value } = event.target;
+          setEmail(value);
+        }}/>
+    <TextField id="outlined-search" label="Myers-Briggs" type="text" variant="outlined" onChange={event => {
+          const { value } = event.target;
+          setMyersBriggs(value);
+        }}/>
+    <DatePicker values={selectedDate} handleDate={setSelectedDate} />
+    <SubmitButton handleClick={handleSubmit}/>
+      </div>
+    </form>
     </div>
   );
 }
--- src/Components/6.Timeline_Page/index.js
+++ src/Components/6.Timeline_Page/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ProgressBar from '../ProgressBar/index';
 import { ThemeProvider } from '../ProgressBar/themeContext';
 import H2 from '../DisplayText/H2Text/index';
+import H1 from '../DisplayText/H1Text/index';
 import congratsData from './congratsData';
 import { useAppContext } from '../../AppContext';
 
@@ -14,22 +15,17 @@ function Timeline() {
     if (currentWeek) {
       let res = congratsData.filter((congratsData) => {
         if (congratsData.week === currentWeek) {
-          console.log(congratsData.message);
           return true;
         }
         return false;
       });
-      console.log(res);
       setcongratsMessage(res[0].message);
-      console.log(`this is your congrats message pls work ${congratsMessage}`);
     }
   }, [currentWeek, congratsMessage]);
 
   return (
     <div>
-      <h1>Your Timeline</h1>
-      <p> (Add in progress bar here)</p>
-      <p> (Add in progress congratulations message here)</p>
+      <H1 text={"Your Timeline"}></H1>
       <H2 text={congratsMessage}></H2>
       <ThemeProvider>
         <ProgressBar />
@@ -40,9 +36,3 @@ function Timeline() {
 
 export default Timeline;
 
-// h2 that is dynamically pulled in from array based on week
-// use reusable h2
-// create array with congratulations week and week
-//import congrats
-//import function to figure out date
-// if statement to show correct statement
