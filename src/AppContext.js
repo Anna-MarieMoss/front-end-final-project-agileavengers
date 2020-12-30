178e256faf2fa00f646f738af232b5ad36aba0a6
= <=>
2020-12-30 17:17:51 +0000

    user added to appContextProvider

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,11 +1,13 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { progressPosition } from './progressFunction';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const AppContext = createContext(null);
 
 export function AppProvider({ children }) {
   const [currentWeek, setCurrentWeek] = useState('week1');
   let startDate = 'Oct 22 2020';
+  const { user } = useAuth0();
 
   useEffect(() => {
     let week = progressPosition(startDate);
@@ -16,6 +18,7 @@ export function AppProvider({ children }) {
     <AppContext.Provider
       value={{
         currentWeek: currentWeek,
+        user: user,
       }}
     >
       {children}
