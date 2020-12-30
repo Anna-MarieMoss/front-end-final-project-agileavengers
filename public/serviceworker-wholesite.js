562c258715022316917dfde9df6a344eabd77e91
= <=>
2020-12-30 10:10:12 +0000

    sw working

--- public/serviceworker-wholesite.js
+++ public/serviceworker-wholesite.js
@@ -4,35 +4,10 @@ const urlsToCache = ['index.html', 'offline.html' ] //offline.html - page shows 
 
 const self = this;
 
-//Install service worker
-// self.addEventListener('install', (event) => {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME)
-//             .then((cache) => {
-//                 console.log("open cache");
-//                 return cache.addAll(urlsToCache);
-//             })
-//     )
-// });
-
 // Install Service - smaller with the self loading cache
 self.addEventListener('install', (event) => {
     console.log('Service Worker: cache installed')
 });
-
-
-//Listen for requests
-// This is based on a weather API - but i think it may be good for any fetch requests etc to out DB
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(() => {
-//                 return fetch(event.request)
-//                     .catch(() => caches.match('offline.html')) //if an error occurs i.e. fetch request not fullfilled use cache HTML
-//             }) // if want to respond with app pages change 'offline.html' to event.request - but need to add these pages to urls to cache
-//     )
-// });
-
 
 //Activate the SW
 self.addEventListener('activate', (event) => {
