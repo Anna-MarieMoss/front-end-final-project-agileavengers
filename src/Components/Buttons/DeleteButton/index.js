c1b6e57767d47b255a522440b05f18764bcae2df
= <=>
2021-01-03 22:06:29 +0000

    another save

--- src/Components/Buttons/DeleteButton/index.js
+++ src/Components/Buttons/DeleteButton/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 
+//Do we need to add the key of the item to be deleted here?
+
 function DeleteButton({deleteColor, handleDelete}) {
   return (
     <DeleteRoundedIcon fontSize="medium" variant="contained" style={{color: deleteColor}} onclick={handleDelete}>
