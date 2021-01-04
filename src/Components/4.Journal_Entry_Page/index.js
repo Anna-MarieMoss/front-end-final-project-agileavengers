11b634fc95d56225210ebe02ea88e9e60938380b
Anna-Marie Moss <ambrown1976@googlemail.com>
2021-01-04 10:51:34 +0000

    removal of old page and connecting new code to the nav bar

--- src/Components/4.Journal_Entry_Page/index.js
+++ src/Components/4.Journal_Entry_Page/index.js
@@ -1,147 +1,186 @@
 import React, { useState, useEffect } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 import './journal.css';
 
 //this will need to link to user iD
 const userId = 1;
 
-function JournalEntry() {
+export default function JournalEntry() {
+  // Code to hold the states of each input...
   const [text, setText] = useState('');
-  const [img, setImg] = useState('');
-  const [vid, setVid] = useState('');
-  const [audio, setAudio] = useState('');
+  const [imgUpload, setImgUpload] = useState('');
+  const [vidUpload, setVidUpload] = useState('');
+  const [audioUpload, setAudioUpload] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert('A new post has been added to your journal.');
+  // These states hold the super long DataURL strings of the file...
+  const [previewImgSource, setPreviewImgSource] = useState();
+  const [previewVidSource, setPreviewVidSource] = useState();
+  const [previewAudioSource, setPreviewAudioSource] = useState();
+
+  const handleImageInputChange = (e) => {
+    // Storing input value into a variable...
+    const imgFile = e.target.files[0];
+    // Preview function to show the img /vid /audio file that has been selected for upload.
+    previewImage(imgFile);
   };
 
-  //Draft post request
+  const handleVideoInputChange = (e) => {
+    const videoFile = e.target.files[0];
+    previewVideo(videoFile);
+  };
 
-  useEffect(() => {
-    if (text) {
-      async function postJournalEntry() {
-        const res = await fetch(
-          // neeed to actual API address
-          `http://localhost:5000/posts`,
-          {
-            method: 'POST',
-            headers: { 'content-type': 'application/JSON' },
-            body: JSON.stringify({
-              user_id: userId,
-              post: text,
-              // ask back end to add columns to tables
-              //  image: img,
-              //  audio: audio,
-              //  video: vid
-            }),
-          }
-        );
-        const data = await res.json();
-        console.log(data);
-      }
-      postJournalEntry();
+  const handleAudioInputChange = (e) => {
+    const audioFile = e.target.files[0];
+    previewAudio(audioFile);
+  };
+
+  const previewImage = (imgFile) => {
+    //This section show the user what file they have selected by creating a reader...
+    const reader = new FileReader(); // (nb. this is a built in javascript api)
+    reader.readAsDataURL(imgFile); // then converts img into a DataURL / string
+    reader.onloadend = () => {
+      setPreviewImgSource(reader.result);
+      // The previewImgSource state will now be able to display the uploaded file when used at the bottom of the return code.
+    };
+  };
+
+  const previewVideo = (videoFile) => {
+    //show the user what they have selected by creating a reader...
+    const reader = new FileReader(); // this is a built in javascript api
+    reader.readAsDataURL(videoFile); // converts img into a URL / string
+    reader.onloadend = () => {
+      setPreviewVidSource(reader.result);
+    };
+  };
+
+  const previewAudio = (audioFile) => {
+    //show the user what they have selected by creating a reader...
+    const reader = new FileReader(); // this is a built in javascript api
+    reader.readAsDataURL(audioFile); // converts img into a URL / string
+    reader.onloadend = () => {
+      setPreviewAudioSource(reader.result);
+    };
+  };
+
+  // function to submit the text and uploaded file's to the server...
+  const handleSubmitFile = (e) => {
+    console.log('submitting');
+    e.preventDefault();
+    postJournalEntry(
+      userId,
+      text,
+      previewImgSource,
+      previewVidSource,
+      previewAudioSource
+    );
+  };
+
+  async function postJournalEntry(
+    userId,
+    text,
+    previewImgSource,
+    previewVidSource,
+    previewAudioSource
+  ) {
+    try {
+      const res = await fetch(`http://localhost:5000/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+          user_id: userId,
+          text: text,
+          image: previewImgSource,
+          video: previewVidSource,
+          audio: previewAudioSource,
+        }),
+        headers: { 'content-type': 'application/JSON' },
+      });
+
+      console.log(res);
+
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
     }
-  }, [text]);
-  // , img, audio, vid]);
-
-  //
+  }
 
   return (
     <div className='wrapper'>
       <h1>How was your day today?</h1>
       <h1>What did you learn?</h1>
       <div className='container'>
-        {/* <br></br> */}
-        <div className='preview'>
+        <form onSubmit={handleSubmitFile}>
           <h2>Create post:</h2>
           <h2>{text}</h2>
-          <h4>{img}</h4> <h4>{vid}</h4> <h4>{audio}</h4>
-        </div>
-        <form action='/journalMedia' onSubmit={handleSubmit}>
-          <fieldset>
-            <textarea
-              className='journalNote'
-              rows='4'
-              cols='30'
-              //add onchange event here
-              placeholder='Add your journal note here...'
-              type='text'
-              onChange={(event) => setText(event.target.value)}
-            ></textarea>
-            {/* </form> */}
-
-            <p> (Add photos, images and voice recordings here!)</p>
-
-            {/* <form> */}
-            <label for='img'>Select image:</label>
-            <input
-              type='file'
-              id='img'
-              name='img'
-              accept='image/*'
-              onChange={(event) => setImg(event.target.files[0])}
-            />
-            <input className='align-right' type='submit' value='+' />
-            {/* </form> */}
-            <br></br>
-            {/* <form> */}
-            <label for='video'>Select video:</label>
-            <input
-              type='file'
-              id='video'
-              name='video'
-              accept='video/*'
-              onChange={(event) => setVid(event.target.value)}
-            />
-            <input type='submit' value='+' />
-            {/* </form> */}
-            <br></br>
-            {/* <form> */}
-            <label for='audio'>Select audio:</label>
-            <input
-              type='file'
-              id='audio'
-              name='audio'
-              accept='audio/*'
-              onChange={(event) => setAudio(event.target.value)}
-            />
-            <input className='align-right' type='submit' value='+' />
-          </fieldset>
+          <input
+            type='text'
+            name='text'
+            onChange={(event) => setText(event.target.value)}
+            value={text}
+            className='form-input'
+          />
           <br></br>
-          <button className='journalButton' type='submit'>
-            Save
+          <label for='img'>Select image:</label>
+          <input
+            type='file'
+            name='image'
+            accept='image/*'
+            onChange={handleImageInputChange}
+            value={imgUpload}
+            className='form-input'
+          />
+          <br></br>
+          <label for='video'>Select video:</label>
+          <input
+            type='file'
+            name='video'
+            accept='video/*'
+            onChange={handleVideoInputChange}
+            value={vidUpload}
+            className='form-input'
+          />
+          <br></br>
+          <label for='audio'>Select audio:</label>
+          <input
+            type='file'
+            name='audio'
+            accept='audio/*'
+            onChange={handleAudioInputChange}
+            value={audioUpload}
+            className='form-input'
+          />
+          <button className='btn' type='submit'>
+            Submit
           </button>
         </form>
         <br></br>
-      </div>
-
-      {/* <div>
-        <br></br>
-        <form action='#' id='myform' enctype='multipart/form-data'>
-          <input
-            type='file'
-            id='capture'
-            accept='image/*,video/*,audio/*'
-            capture
-            multiple
+        {/* Media Upload previews for image, video and audio player */}
+        {previewImgSource && (
+          <img
+            src={previewImgSource}
+            alt='chosenImg'
+            style={{ width: '70%' }}
           />
-
-          <br />
-          
-        <input type='submit' value='Add media' />
-        </form>
-        <p>
-          <img src='' id='img' alt='from phone' />
-        </p>
-        <p>
-          <audio src='' id='audio' controls></audio>
-        </p>
-        <p>
-          <video src='' id='video' controls></video>
-        </p>
-      </div> */}
+        )}
+        <br></br>
+        {previewVidSource && (
+          <video
+            src={previewVidSource}
+            alt='chosenVideo'
+            style={{ width: '70%' }}
+            controls
+          />
+        )}
+        {previewAudioSource && (
+          <ReactAudioPlayer
+            src={previewAudioSource}
+            alt='chosenAudio'
+            style={{ width: '70%' }}
+            autoplay
+            controls
+          />
+        )}
+      </div>
     </div>
   );
 }
-
-export default JournalEntry;
