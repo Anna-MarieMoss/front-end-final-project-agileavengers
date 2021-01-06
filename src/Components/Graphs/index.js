df87b50dfd7e07d210edba2dc71b522133855394
Jeremy Law <jeremylaw2308@gmail.com>
2021-01-06 14:57:23 +0000

    graph working

--- src/Components/Graphs/index.js
+++ src/Components/Graphs/index.js
@@ -18,8 +18,7 @@
 //   { day: 'wednesday', mood: 3 },
 //   { day: 'thursday', mood: 1 },
 //   { day: 'friday', mood: 3 }
-  
-  
+
 // ];
 
 // const wweklyData = [
@@ -28,13 +27,13 @@
 //     { day: 'three', mood: 3 },
 //     { day: 'four', mood: 1 },
 //     { day: 'five', mood: 3 }
-    
+
 //   ];
 // //
 
 // async function moodChart(
 //   userId,
-//   mood, 
+//   mood,
 //   date
 // ) {
 //   try {
@@ -57,11 +56,10 @@
 
 // //
 
-  
 //     return (
 //       <Paper>
 //         <Chart
-          
+
 //         >
 //           <ArgumentAxis />
 //           <ValueAxis max={7} />
@@ -77,45 +75,38 @@
 //     );
 //   }
 
-
 // export default Graph
 
-import React, { useEffect, useRef, useState } from "react";
-import Chartjs from "chart.js";
+import React, { useEffect, useRef, useState } from 'react';
+import Chartjs from 'chart.js';
 
-//Backend URL  
+//Backend URL
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 //Temp userId
 const userId = 1;
 
-
-
-const Graph= () => {
+const Graph = () => {
   const chartContainer = useRef(null);
   const [chartInstance, setChartInstance] = useState(null);
-  const [graphData, setGraphData] = useState([0,0,0,0,0]);
-  const [showGraph, setShowGraph] = useState(false)
-  
-//graph
+  const [graphData, setGraphData] = useState([0, 0, 0, 0, 0]);
+  const [showGraph, setShowGraph] = useState(false);
 
-const randomInt = () => Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+  //graph
 
+  const randomInt = () => Math.floor(Math.random() * (5 - 1 + 1)) + 1;
 
-
-
-  function handleMood(){
-   setShowGraph(true)
+  function handleMood() {
+    setShowGraph(true);
     console.log(`graph should be showing`);
   }
-
 
   useEffect(() => {
     if (chartContainer && chartContainer.current) {
       const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
       setChartInstance(newChartInstance);
     }
-  }, [chartContainer]);
+  }, [chartContainer, graphData]);
 
   const updateDataset = (datasetIndex, newData) => {
     chartInstance.data.datasets[datasetIndex].data = newData;
@@ -128,78 +119,100 @@ const randomInt = () => Math.floor(Math.random() * (10 - 1 + 1)) + 1;
       randomInt(),
       randomInt(),
       randomInt(),
-      randomInt()
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
     ];
     updateDataset(0, data);
   };
 
   useEffect(() => {
-   
-  async function getMood() {
-    
+    async function getMood() {
       const res = await fetch(`${BACKEND_URL}/moods/1`);
-     const data = await res.json();
-    // console.log( `data is  ${JSON.stringify(data)}`);
-    
-    
-    console.log(`data payload is `,data.payload)
-    // console.log(`data is ${JSON.stringify(data.payload[0].mood)}`)
-    setGraphData(data.payload)
-    console.log(`graphData state is`,graphData)
-    chartConfig.data.datasets[0].data = graphData.map(x => x.mood)
-  }
-  
+      const data = await res.json();
+      // console.log( `data is  ${JSON.stringify(data)}`);
 
-  getMood();
-  
-}, [showGraph]);
+      console.log(`data payload is `, data.payload);
+      // console.log(`data is ${JSON.stringify(data.payload[0].mood)}`)
+      setGraphData(data.payload);
+      console.log(`graphData state is`, graphData);
+      //chartConfig.data.datasets[0].data = graphData.map((x) => x.mood);
+    }
 
-let chartConfig = {
-    type: "bar",
+    getMood();
+  }, [showGraph]);
+
+  let chartConfig = {
+    type: 'bar',
     data: {
-      labels: ["Day One", "Day Two", "Day Three", "Day Four", "Day Five"],
+      labels: [
+        'Day One',
+        'Day Two',
+        'Day Three',
+        'Day Four',
+        'Day Five',
+        'Day One',
+        'Day Two',
+        'Day Three',
+        'Day Four',
+        'Day Five',
+      ],
       datasets: [
         {
-          label: "Mood",
-          data: [0,0,0,0,0],
+          label: 'Mood',
+          data: graphData.map((x) => x.mood),
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
           ],
           borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
           ],
-          borderWidth: 1
-        }
-      ]
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       scales: {
         yAxes: [
           {
             ticks: {
-              beginAtZero: true
-            }
-          }
-        ]
-      }
-    }
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
   };
   //
   return (
     <div>
       <button onClick={onButtonClick}>Switch View</button>
       <button onClick={handleMood}>Get Mood Data</button>
-      <canvas ref={chartContainer} style={{width: '100em', height: '100em'}}/>
+      <canvas
+        ref={chartContainer}
+        style={{ width: '100em', height: '100em' }}
+      />
     </div>
   );
 };
