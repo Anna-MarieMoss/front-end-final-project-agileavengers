bb911f07b29c89cae30fbc420748c332bde8b506
= <=>
2021-01-13 10:56:06 +0000

    logo

--- src/Components/Pictures/SocLogo/index.js
+++ src/Components/Pictures/SocLogo/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import pic from "./SoC_Logo.png"
 
-function SoCLogo({style}) {
-  return <img id='soc-logo' style={style} src={pic} alt="Soc Logo" width='7em'/>;
+function SoCLogo() {
+  return <img id='soc-logo' src={pic} alt="Soc Logo" width='7em'/>;
 }
 
 export default SoCLogo;
